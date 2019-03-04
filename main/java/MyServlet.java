401909b29186bfbdcf825961fa212791e9591274
Nikolay <pervakmv@elas.kiev.ua>
2019-03-04 16:23:05 +0200

    MyFirstServlat

--- main/java/MyServlet.java
+++ main/java/MyServlet.java
@@ -1,4 +1,5 @@
-import javax.servlet.ServletException;
+import javax.servlet.*;
+//import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,7 +8,7 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
         resp.getWriter().println("My first servlet ");
     }
 
